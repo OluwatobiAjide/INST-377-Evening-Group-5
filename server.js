46d2cdb99731ab972ff3e09910652fbe4e053875
Oluwatobi Ajide <biajide@gmail.com>
2020-04-18 20:55:50 -0400

    server works now

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var app = express()
-
-app.set('port', (process.env.PORT || 5000))
+var port = process.env.PORT || 8000
 app.use(express.static(__dirname + '/public'))
 
 app.get('/api', function(request, response) {
   response.send('Hello World!')
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.listen(port, function() {
+  console.log("Node app is running at localhost:" + port)
+}) 
