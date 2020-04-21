a110d39787305e712a084d62ad32a3f748f25300
Oluwatobi Ajide <biajide@gmail.com>
2020-04-20 23:18:29 -0400

    created server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
-var app = express()
-var port = process.env.PORT || 8000
-app.use(express.static(__dirname + '/public'))
+var mongoose = require('mongoose');
+require('dotenv/config')
+var app = express();
+var port = process.env.PORT || 8000;
+app.use(express.static(__dirname + '/public'));
+
+
 
 app.listen(port, function() {
-  console.log("Node app is running at localhost:" + port)
-}) 
+  console.log("Node app is running at localhost:" + port);
+});
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true } , ()=>console.log("Database Connected"));
