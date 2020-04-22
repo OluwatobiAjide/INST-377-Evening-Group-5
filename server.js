9f075eaf783c9790e9cd807aca91c53f9e86ebfd
Oluwatobi Ajide <biajide@gmail.com>
2020-04-22 13:22:41 -0400

    ^

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var mongoose = require('mongoose');
 const fetch = require('node-fetch');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv/config')
@@ -13,24 +12,25 @@ app.listen(port, function() {
   console.log("Node app is running at localhost:" + port);
 });
 
+
+
 //Connect to DB
-// var info;
-// const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-// const uri = "mongodb+srv://admin:snw1B9IdJvv0Ds0m@cluster0-ufrmk.mongodb.net/test?retryWrites=true&w=majority";
-// fetch(baseURL).then((r) => r.json()).then((data) => {
-//   info = data;
-// })
-// MongoClient.connect(uri, { useNewUrlParser: true },{ useUnifiedTopology: true },(err,client) => {
-//   if  (err) {
-//     console.error(err);
-//     return;
-//   }
-//   const db = client.db('data')
-//   const collection = db.collection('restaurant')
-//   collection.insertMany(info, (err,result) => {
-//     console.log(result);
-//   })
+
+const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const uri = "mongodb+srv://admin:snw1B9IdJvv0Ds0m@cluster0-ufrmk.mongodb.net/test?retryWrites=true&w=majority";
+fetch(baseURL).then((r) => r.json()).then((data) => {
+  MongoClient.connect(uri, { useNewUrlParser: true },{ useUnifiedTopology: true },(err,client) => {
+    if  (err) {
+      console.error(err);
+      return;
+    }
+    const db = client.db('data')
+    const collection = db.collection('restaurant')
+    data.forEach(function(item){
+        collection.insertOne(item);
+    });
+    
+  });
   
-//     client.close
-// });
+})
 
