fa4f421b2dc39e5e219925a98c41e46f4314bdc4
Oluwatobi Ajide <biajide@gmail.com>
2020-04-25 17:39:53 -0400

    docs update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# INST-377-Evening-Group-5
+### Title: Restaurant Rater
+* Description of your project
+* Link to the Heroku, Netlify, or Digital Ocean instance where your application can be used
+* Description of target browsers (iOS? Android? Which ones?)
+* Link to User Manual
+* Link to Developer Manual
--- server.js
+++ server.js
@@ -1,17 +1,43 @@
-const express = require("express");
-const fetch = require("node-fetch");
-//const sqlite3 = require("sqlite3").verbose();
-const MongoClient = require('mongodb').MongoClient;
-require("dotenv/config");
-var app = express();
-var port = process.env.PORT || 8000;
-app.use(express.static(__dirname + "/public"));
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+const express = require('express');
+const fetch = require('node-fetch');
+// const sqlite3 = require("sqlite3").verbose();
+const { MongoClient } = require('mongodb');
+require('dotenv/config');
 
-app.listen(port, function () {
-  console.log("Node app is running at localhost:" + port);
+const app = express();
+const port = process.env.PORT || 8000;
+app.use(express.static(`${__dirname}/public`));
+
+app.listen(port, () => {
+  console.log(`Node app is running at localhost:${port}`);
 });
 
-//Connect to DB
+const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$limit=36000';
+const uri = process.env.MONGODB_URL;
+fetch(baseURL)
+  .then((r) => r.json())
+  .then((data) => {
+    MongoClient.connect(uri, { useNewUrlParser: true },
+      { useUnifiedTopology: true }, (err, client) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        const db = client.db('data');
+        const collection = db.collection('restaurant');
+        
+        collection.insertMany(data, (error, res) => {
+          if (error) throw error;
+          console.log(`Number of documents inserted: ${res.insertedCount}`);
+          client.close();
+        });
+      });
+  });
+
+
+// Connect to DB
 // const db = new sqlite3.Database("./db/food_place.db", (err) => {
 //   if (err) {
 //     return console.error(err.message);
@@ -27,42 +53,20 @@ app.listen(port, function () {
 //   for (var key in data){
 //     var info = data[key];
 //     const param = [info.establishment_id, info.name , info.category , info.inspection_date , info.inspection_results , info.city , info.state , info.zip , info.address_line_1 , info.address_line_2 , info.inspection_type , info.owner , info.type]
-    
+
 //     const sql = "INSERT INTO food_inspection VALUES(:establishment_id, :name , :category , date(:inspection_date) ,:inspection_results, :city , :state , :zip , :address_line_1 , :address_line_2 , :inspection_type , :owner , :type)"
 
 //     db.run(sql,param,(err)=>{
 //       if(err){
 //         return console.error(err.message);
 //       }
-      
+
 //     });
-    
-    
+
+
 //   }
- 
+
 //   console.log("Insert Complete");
-  
+
 // });
 // db.close();
-
-const baseURL = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$limit=36000";
-const uri = process.env.MONGODB_URL;
- fetch(baseURL)
-  .then((r) => r.json())
-  .then((data) => {
-   
-     MongoClient.connect(uri, { useNewUrlParser: true },{ useUnifiedTopology: true },(err,client) => {
-      if  (err) {
-        console.error(err);
-        return;
-      }
-      const db = client.db('data')
-      const collection = db.collection('restaurant')
-        collection.insertMany(data,function(err, res) {
-          if (err) throw err;
-          console.log("Number of documents inserted: " + res.insertedCount);
-          client.close();
-        });
-        
-    });
-  });
