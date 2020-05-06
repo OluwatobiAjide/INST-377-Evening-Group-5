4680137e747bb43f0e259c72c294c20b6521ebf5
RuiqiLai0210 <60375884+RuiqiLai0210@users.noreply.github.com>
2020-05-06 16:36:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,75 +4,68 @@
 /* eslint-disable no-console */
 const express = require('express');
 const fetch = require('node-fetch');
-// const sqlite3 = require("sqlite3").verbose();
-const { MongoClient } = require('mongodb');
+const sqlite3 = require('sqlite3').verbose();
 require('dotenv/config');
 
 const app = express();
 const port = process.env.PORT || 8000;
 app.use(express.static(`${__dirname}/public`));
 
-
 app.listen(port, () => {
   console.log(`Node app is running at localhost:${port}`);
 });
 
 const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$limit=36000';
-const uri = process.env.MONGODB_URL;
+
+const db = new sqlite3.Database('./db/food_place.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connnected to the food_place database');
+});
 
 fetch(baseURL)
   .then((r) => r.json())
   .then((data) => {
-    MongoClient.connect(uri, { useNewUrlParser: true },
-      { useUnifiedTopology: true }, async (err, client) => {
-        if (err) {
-          console.error(err);
-          return;
+    db.serialize(() => {
+      db.run(
+        'CREATE TABLE IF NOT EXISTS food_inspection(establishment_id integer  , name text , category text , inspection_date text, inspection_result text, city text, state text, zip integer, address_line_1 text, address_line_2 text, inspection_type text, owner text, establishment_type text )',
+      );
+
+      for (const key in data) {
+        const info = data[key];
+        if (
+          info.category === 'Restaurant'
+          || info.category === 'Carry-out'
+          || info.category === 'Fast Food'
+        ) {
+          const param = [
+            info.establishment_id,
+            info.name,
+            info.category,
+            info.inspection_date,
+            info.inspection_results,
+            info.city,
+            info.state,
+            info.zip,
+            info.address_line_1,
+            info.address_line_2,
+            info.inspection_type,
+            info.owner,
+            info.type,
+          ];
+
+          const sql = 'INSERT INTO food_inspection VALUES(:establishment_id, :name , :category , date(:inspection_date) ,:inspection_results, :city , :state , :zip , :address_line_1 , :address_line_2 , :inspection_type , :owner , :type)';
+
+          db.run(sql, param, (err) => {
+            if (err) {
+              return console.error(err.message);
+            }
+          });
         }
-        const db = await client.db('data');
-        const collection = await db.collection('restaurant');
-        const filtered = data.filter((key) => (key.category === 'Restaurant' || key.category === 'Carry-out'
-           || key.category === 'Fast Food'));
-        collection.insertMany(filtered, (error, result) => {
-          if (error) throw error;
-          console.log(result.insertedCount);
-          client.close();
-        });
-        console.log(new Date(data[0].inspection_date).getFullYear());
-      });
+      }
+
+      console.log('Insert Complete');
+    });
+    db.close();
   });
-
-
-// Connect to DB
-// const db = new sqlite3.Database("./db/food_place.db", (err) => {
-//   if (err) {
-//     return console.error(err.message);
-//   }
-//   console.log("Connnected to the food_place database");
-// });
-
-// db.serialize(()=>{
-//   db.run(
-//     "CREATE TABLE IF NOT EXISTS food_inspection(establishment_id integer  , name text , category text , inspection_date text, inspection_result text, city text, state text, zip integer, address_line_1 text, address_line_2 text, inspection_type text, owner text, establishment_type text )"
-//   );
-
-//   for (var key in data){
-//     var info = data[key];
-//     const param = [info.establishment_id, info.name , info.category , info.inspection_date , info.inspection_results , info.city , info.state , info.zip , info.address_line_1 , info.address_line_2 , info.inspection_type , info.owner , info.type]
-
-//     const sql = "INSERT INTO food_inspection VALUES(:establishment_id, :name , :category , date(:inspection_date) ,:inspection_results, :city , :state , :zip , :address_line_1 , :address_line_2 , :inspection_type , :owner , :type)"
-
-//     db.run(sql,param,(err)=>{
-//       if(err){
-//         return console.error(err.message);
-//       }
-
-//     });
-
-
-//   }
-
-//   console.log("Insert Complete");
-
-// });
-// db.close();
