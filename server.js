1b400ca338ef4e08ff039dfe89a9e2dbad05fc3a
Oluwatobi Ajide <biajide@gmail.com>
2020-05-06 22:15:26 -0400

    database fixed

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 /* eslint-disable no-console */
 const express = require('express');
 const fetch = require('node-fetch');
+const { MongoClient } = require('mongodb');
 const sqlite3 = require('sqlite3').verbose();
 require('dotenv/config');
 
@@ -17,59 +18,43 @@ app.listen(port, () => {
 
 const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?$limit=36000';
 
-const db = new sqlite3.Database('./db/food_place.db', (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connnected to the food_place database');
-});
-
-
+const uri = process.env.MONGODB_URL;
 fetch(baseURL)
   .then((r) => r.json())
   .then((data) => {
-    db.serialize(() => {
-      db.run(
-        'CREATE TABLE IF NOT EXISTS food_inspection(establishment_id integer  , name text , category text , inspection_date text, inspection_result text, city text, state text, zip integer, address_line_1 text, address_line_2 text, inspection_type text, owner text, establishment_type text )',
-      );
-
-      for (const key in data) {
-        const info = data[key];
-        if (
-          info.category === 'Restaurant'
-          || info.category === 'Carry-out'
-          || info.category === 'Fast Food'
-        ) {
-          const param = [
-            info.establishment_id,
-            info.name,
-            info.category,
-            info.inspection_date,
-            info.inspection_results,
-            info.city,
-            info.state,
-            info.zip,
-            info.address_line_1,
-            info.address_line_2,
-            info.inspection_type,
-            info.owner,
-            info.type,
-          ];
-
-          const sql = 'INSERT INTO food_inspection VALUES(:establishment_id, :name , :category , date(:inspection_date) ,:inspection_results, :city , :state , :zip , :address_line_1 , :address_line_2 , :inspection_type , :owner , :type)';
-
-          db.run(sql, param, (err) => {
-            if (err) {
-              return console.error(err.message);
-            }
-          });
+    MongoClient.connect(uri, { useNewUrlParser: true },
+      { useUnifiedTopology: true }, async (err, client) => {
+        if (err) {
+          console.error(err);
+          return;
         }
-      }
-
-      console.log('Insert Complete');
-    });
+        const db = await client.db('data');
+        const collection = await db.collection('restaurant');
+        const filtered = data.filter((key) => (key.category === 'Restaurant' || key.category === 'Carry-out'
+           || key.category === 'Fast Food'));
+        collection.insertMany(filtered, (error, result) => {
+          if (error) throw error;
+          console.log(`Number of documents inserted: ${result.insertedCount}`);
+          console.log(result.insertedCount);
+          client.close();
+        });
+      });
   });
 app.route('/api').get((req, res) => {
-  const result = db.run('SELECT * From food_inspection');
-  res.send(result);
+  console.log('get request');
+  MongoClient.connect(uri, { useNewUrlParser: true }, async (err, client) => {
+    if (err) throw err;
+
+    const db = client.db('data');
+
+    await db.collection('restaurant').find({}).toArray().then((data) => {
+      res.send(data);
+    })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        client.close();
+      });
+  });
 });
