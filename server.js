b93bd7f3d8979e3234ee98bdae95c9d41035ed48
RuiqiLai0210 <60375884+RuiqiLai0210@users.noreply.github.com>
2020-05-09 19:02:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,12 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const { MongoClient } = require('mongodb');
-const sqlite3 = require('sqlite3').verbose();
 require('dotenv/config');
 
 const app = express();
 const port = process.env.PORT || 8000;
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.listen(port, () => {
@@ -30,9 +31,13 @@ fetch(baseURL)
         }
         const db = await client.db('data');
         const collection = await db.collection('restaurant');
-        const filtered = data.filter((key) => (key.category === 'Restaurant' || key.category === 'Carry-out'
+        collection.remove();
+        const filtered = await data.filter((key) => (key.category === 'Restaurant' || key.category === 'Carry-out'
            || key.category === 'Fast Food'));
-        collection.insertMany(filtered, (error, result) => {
+        const filter = await filtered.filter((key) => new Date(key.inspection_date).getFullYear() === 2018
+            || new Date(key.inspection_date).getFullYear() === 2019
+            || new Date(key.inspection_date).getFullYear() === 2020);
+        collection.insertMany(filter, (error, result) => {
           if (error) throw error;
           console.log(`Number of documents inserted: ${result.insertedCount}`);
           console.log(result.insertedCount);
@@ -40,21 +45,34 @@ fetch(baseURL)
         });
       });
   });
-app.route('/api').get((req, res) => {
-  console.log('get request');
+app.route('/api').put((req, res) => {
+  console.log(req.body);
   MongoClient.connect(uri, { useNewUrlParser: true }, async (err, client) => {
     if (err) throw err;
-
     const db = client.db('data');
-
-    await db.collection('restaurant').find({}).toArray().then((data) => {
-      res.send(data);
-    })
-      .catch((error) => {
-        console.log(error);
+    if (req.body.category === 'Zip Code' && req.body.searchBar.match(/^\d{5}$/) && !req.body.searchBar.match(/(\d{1,}) [a-zA-Z0-9\s]+(\.)? [a-zA-Z]+/)) {
+      await db.collection('restaurant').find({ zip: req.body.searchBar }).toArray().then((data) => {
+        res.send(data);
       })
-      .finally(() => {
-        client.close();
-      });
+        .catch((error) => {
+          console.log(error);
+        });
+    } else if (req.body.category === 'Address' && req.body.searchBar.match(/(\d{1,}) [a-zA-Z0-9\s]+(\.)? [a-zA-Z]+/)) {
+      await db.collection('restaurant').find({ address_line_1: req.body.searchBar }).toArray().then((data) => {
+        res.send(data);
+      })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else if (req.body.category === 'Restaurant Name' && !req.body.searchBar.match(/^\d{5}$/) && !req.body.searchBar.match(/(\d{1,}) [a-zA-Z0-9\s]+(\.)? [a-zA-Z]+/)) {
+      await db.collection('restaurant').find({ name: req.body.searchBar }).toArray().then((data) => {
+        res.send(data);
+      })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      res.send({ message: 'error' });
+    }
   });
 });
